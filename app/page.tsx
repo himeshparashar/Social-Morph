746ddacb0c8f15bc9b3e89f35643fc0fdcc1ed2f
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-08-20 02:26:44 +0530

    Navbar and background built

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from '@/components/landing-page/Hero'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      Lakshit making landing page
+      <Hero/>
     </div>
   )
 }
