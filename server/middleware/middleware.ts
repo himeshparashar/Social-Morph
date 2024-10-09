e684548cc4449a8662cb4caf5d31ab101ba5ce5b
Akaimnky <bhairabm1908@gmail.com>
2024-10-09 19:50:31 +0530

    fixed some minor changes

--- server/middleware/middleware.ts
+++ server/middleware/middleware.ts
@@ -1,6 +1,6 @@
 import { Request as ExpressRequest, Response, NextFunction } from "express";
 import jwt, { JwtPayload, TokenExpiredError } from "jsonwebtoken";
-import config from "../config/config";
+require("dotenv").config();
 import { TokenData } from "../interfaces/tokenInterface";
 import { PrismaClient } from "@prisma/client";
 import ErrorHandler from "../utils/ErrorHandler";
@@ -74,37 +74,6 @@ export const isAuthenticated = CatchAsyncError(
     }
   }
 );
-
-export const protectAdmin = async (
-  req: RequestWithUserId,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized: No token provided",
-      });
-    }
-
-    const decoded = jwt.verify(token, config.JWT_SECRET) as TokenData;
-    const id = decoded._id;
-    const user = await prisma.user.findUnique({ where: { id } });
-    if (user.id !== id || user.isVerified !== true || user.role !== "ADMIN") {
-      throw new Error("Unauthorized: No token provided");
-    }
-    req.id = user.id;
-    next();
-  } catch (error) {
-    console.error("Authentication error:", error);
-    return res.status(401).json({
-      success: false,
-      message: "Unauthorized: Invalid token",
-    });
-  }
-};
 
 export const authorizeRoles = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
