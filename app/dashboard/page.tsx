78388b7795e0ff5e3a528eee379a85d10031e830
sezallagwal <sezal1470@gmail.com>
2024-10-18 12:30:47 +0530

    added UserButton to user profile

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,8 +4,21 @@ import React from "react";
 import { useUser } from "@clerk/nextjs";
 import { CalendarDays, Lightbulb, BarChart3, BookMarked, Plus, Search, Edit3 } from "lucide-react";
 import Image from "next/image";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
-export default function Dashboard() {
+const HelpIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height="17px" viewBox="0 -960 960 960" width="17px" fill="#666666"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z"/></svg>
+  )
+}
+const NotificationIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height="17px" viewBox="0 -960 960 960" width="17px" fill="#666666"><path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160ZM480-80q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80Z"/></svg>
+  )
+}
+
+
+export default function Dashboard({children} : {children: React.ReactNode}) {
   const { isLoaded, isSignedIn, user } = useUser();
 
   if (!isLoaded) return <div>Loading...</div>;
@@ -50,13 +63,29 @@ export default function Dashboard() {
         {/* User Information */}
         <div className="absolute bottom-0 p-4 border-t w-full bg-gray-50">
           <div className="flex items-center space-x-4">
-            <Image
+            {/* <Image
               src={user.imageUrl || "/placeholder.svg"}
               alt="User"
               width={40}
               height={40}
               className="rounded-full border-2 border-purple-200"
-            />
+            /> */}
+            <SignedIn>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="Notifications"
+                  labelIcon={<NotificationIcon />}
+                  href="/notifications"
+                />
+                <UserButton.Link
+                  label="Help"
+                  labelIcon={<HelpIcon />}
+                  href="/help"
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+            </SignedIn>
             <div>
               <p className="font-medium text-gray-800">{user.fullName || "User Name"}</p>
               <p className="text-sm text-gray-500">{user.primaryEmailAddress?.emailAddress || "user@example.com"}</p>
