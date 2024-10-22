e40af76c377af9ddab2aaa1a43416eff753e03a6
Himesh Parashar <himeshparashar424@gmail.com>
2024-10-23 02:10:50 +0530

    schedule layout made

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -21,13 +21,13 @@ export default function RootLayout({
       <body>
         <div
           className="flex h-screen bg-gray-50 overflow-hidden"
-          style={{
-            backgroundImage: `
-                linear-gradient(to right, rgba(0, 0, 0, 0.05) 1px, transparent 1px),
-                linear-gradient(to bottom, rgba(0, 0, 0, 0.05) 1px, transparent 1px)
-              `,
-            backgroundSize: "40px 40px",
-          }}
+          //   style={{
+          //     backgroundImage: `
+          //         linear-gradient(to right, rgba(0, 0, 0, 0.05) 1px, transparent 1px),
+          //         linear-gradient(to bottom, rgba(0, 0, 0, 0.05) 1px, transparent 1px)
+          //       `,
+          //     backgroundSize: "40px 40px",
+          //   }}
         >
           <Sidebar />
           <div className="flex-1 flex flex-col overflow-hidden">
